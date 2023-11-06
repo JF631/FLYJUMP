1e153a5d5d7d1bf0ab8982a55f002d5c0e738b19
Jakob Faust <jakob.faust@curemannheim.de>
2023-11-07 00:23:22 +0100

    makes frame objects reuseable.
    reuses allocated memory for frames.
    simply add new data to a already existing frame object

--- ljanalyzer/frame.py
+++ ljanalyzer/frame.py
@@ -20,16 +20,74 @@ class Frame():
     Provides methods to annotate a frame object with given pose landmarks and 
     transform the frame data to mediapipe image format.
 
+    Usage
+    -----
+    Objects of this class are re-useable.
+    To update the data in a frame object simply call frame.update(new_frame)
+
+    If you want to make sure no data from the old frame is re-used, call
+    frame.clear() before frame.update(new_frame) 
+
     '''
-    def __init__(self, frame:np.ndarray) -> None:
-        self.__data = frame
+    def __init__(self, frame:np.ndarray | None = ...) -> None:
         self.__right_knee_angle = 0.0
         self.__left_knee_angle = 0.0
         self.__hip_height = 0.0
-        self.__foot_tip = 0.0
+        self.foot_positions: tuple = None
+        if frame is not None:
+            self.update(frame)
 
     def __bool__(self):
         return self.__data is not None
+
+    def update(self, frame:np.ndarray):
+        '''
+        replaces frame data in the current frame object.
+
+        Parameters
+        ----------
+        frame : np.ndarray
+            new frame of shape (height, width, channels)
+        '''
+        self.__data = frame
+    
+    def clear(self):
+        '''
+        clears current frame.
+        '''
+        self.__data = None
+
+    def __calc_knee_angle(self, key_points: tuple):
+        '''
+        calculates knee angle.
+
+        Parameters
+        ----------
+        key_points : tuple (hip, knee, foot)
+            key_points from pose detection.
+            Must contain hip, knee and foot values, each of which must have
+            .x and .y values 
+
+        Returns
+        -------
+        knee_angle : float
+            knee angle in degrees.
+        '''
+        hip, knee, foot = key_points
+        hip_knee_vec = np.array(
+            [
+                knee.x - hip.x, 
+                knee.y - hip.y
+            ], dtype='f4')
+        knee_foot_vec = np.array(
+            [
+                foot.x - knee.x,
+                foot.y - knee.y
+            ], dtype='f4')
+        return 180 - np.rad2deg(
+            np.arccos((np.vdot(hip_knee_vec, knee_foot_vec)) /
+            (np.linalg.norm(hip_knee_vec) *
+            np.linalg.norm(knee_foot_vec))))
 
     def annotate(self, pose_landmarks, as_overlay=True) -> None:
         '''
@@ -47,16 +105,12 @@ class Frame():
         if not as_overlay:
             self.__data = np.zeros_like(self.__data)
         for pose in pose_landmarks:
-            hip, knee, foot = pose[24:30:2]
             self.foot_tip = pose[32]
-            self.__hip_height = hip.y
-            hip_knee_vec = np.array([knee.x - hip.x, knee.y - hip.y])
-            knee_foot_vec = np.array([foot.x - knee.x, foot.y - knee.y])
-            self.__right_knee_angle = 180 - np.rad2deg(
-                np.arccos((np.vdot(hip_knee_vec, knee_foot_vec)) /
-                (np.linalg.norm(hip_knee_vec) *
-                 np.linalg.norm(knee_foot_vec))))
-            cv2.putText(self.__data, str(self.__right_knee_angle), (10, 30),
+            self.foot_positions = pose[-2:]
+            self.__right_knee_angle = self.__calc_knee_angle(pose[24:30:2])
+            self.__left_knee_angle = self.__calc_knee_angle(pose[23:29:2])
+            cv2.putText(self.__data, f"""right: {self.__right_knee_angle:.4f} 
+                        left:{self.__left_knee_angle:.4f}""", (10, 30),
                         cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
             pose_proto = landmark_pb2.NormalizedLandmarkList()
             pose_proto.landmark.extend([
@@ -105,7 +159,25 @@ class Frame():
         return np.array([self.__right_knee_angle, self.__left_knee_angle])
     
     def foot_pos(self):
-        return self.foot_tip
+        '''
+        Foot position matrix.
+
+        Returns
+        -------
+        foot_pos : np.ndarray
+            Foot position matrix of shape (2,2)
+
+        Usage
+        -----
+        The matrix is ordered as follows:
+
+        [[left_foot.x, right_foot.x],
+         [left_foot.y, right_foot.y]]
+        '''
+        return np.array([
+            [self.foot_positions[0].x, self.foot_positions[1].x],
+            [self.foot_positions[0].y, self.foot_positions[1].y]
+        ])
 
     def centroid_height(self) -> float:
         '''
