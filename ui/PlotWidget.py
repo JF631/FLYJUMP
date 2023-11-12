7ece1164fde5c096b063646b5a9d6177c47c35dc
Jakob <software_jaf@mx442.de>
2023-11-12 01:09:34 +0100

    adds plot legend

--- ui/PlotWidget.py
+++ ui/PlotWidget.py
@@ -1,7 +1,7 @@
 from PyQt5.QtGui import QPen
 from PyQt5.QtWidgets import QWidget, QVBoxLayout
 from PyQt5.QtCore import Qt, pyqtSlot
-from qwt import QwtPlot, QwtPlotCurve
+from qwt import QwtPlot, QwtPlotCurve, QwtLegend
 import numpy as np
 
 from ljanalyzer.video import VideoSignals
@@ -12,10 +12,12 @@ class PlotWidget(QWidget):
         signals.update_frame_parameters.connect(self.set_data)
         # Create a QwtPlot widget
         self.plot = QwtPlot(self)
+        self.plot_legend = QwtLegend()
+        self.plot.insertLegend(self.plot_legend, QwtPlot.BottomLegend)
         self.l_foot_y = np.array([])
         self.r_foot_y = np.array([])
-        self.l_foot_curve = QwtPlotCurve("My Curve")
-        self.r_foot_curve = QwtPlotCurve("right foot")
+        self.l_foot_curve = QwtPlotCurve("Left foot")
+        self.r_foot_curve = QwtPlotCurve("Right foot")
         self.r_foot_curve.setTitle("right foot")
         l_foot_pen = QPen(Qt.red)
         self.l_foot_curve.setPen(l_foot_pen)
@@ -35,7 +37,8 @@ class PlotWidget(QWidget):
 
     @pyqtSlot(np.ndarray)
     def set_data(self, data: np.ndarray):
-        # Set data for the curve
+        if np.any(data < 0.0) or np.any(data > 1.0):
+            return
         self.r_foot_y = np.append(self.r_foot_y, data[1,0])
         self.l_foot_y = np.append(self.l_foot_y, data[1,1])
         self.l_foot_curve.setData(np.arange(len(self.l_foot_y)), self.l_foot_y)
