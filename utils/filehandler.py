6d8e42d89b96f49f6bc6f9394edf7aafd8d61aff
Jakob <software_jaf@mx442.de>
2024-01-21 21:45:14 +0100

    can now return all analysis results

--- utils/filehandler.py
+++ utils/filehandler.py
@@ -9,10 +9,9 @@ import os
 from datetime import datetime
 
 import h5py
+import numpy as np
 
 from ljanalyzer.frame import Frame
-import matplotlib.pyplot as plt
-import numpy as np
 
 class ParameterFile():
     '''
@@ -89,6 +88,19 @@ class ParameterFile():
                 frame_group.create_dataset("hip_height",
                                             data=data['hip_height'])
         self.__frame_data.clear()
+    
+    def get_video_path(self):
+        '''
+        returns video path to current analysis file.
+        Assumes that the video is in the same directory as the analysis file.
+        
+        Returns
+        -------
+        path : str
+            output analysis file path
+            (e.g. 'C:/FLYJUMP/analysis/2023-01-01/vid0.mp4').
+        '''
+        return os.path.splitext(self.__file_path)[0] + '.mp4'
 
     def save(self, frame: Frame):
         '''
@@ -118,6 +130,21 @@ class ParameterFile():
         with h5py.File(file_name, 'a') as param_file:
             param_file.attrs[key] = metadata
 
+    def get_takeoff_frame(self)->int:
+        '''
+        get takeoff frame number from parameter file.
+        
+        Returns
+        -------
+        takeoff_frame : int
+            number in which the takeoff has been detected
+        '''
+        file_name = self.get_path()
+        rtrn = -1
+        with h5py.File(file_name, 'a') as param_file:
+            rtrn = param_file.attrs.get('takeoff', -1)
+        return rtrn
+
     def close(self):
         '''
         Saves all remaining frames to file.
@@ -126,13 +153,29 @@ class ParameterFile():
         '''
         print("saving last data..")
         self.__write_to_file()
-        # self.load(file_name)
     
     def load(self):
+        '''
+        loads parameter file from disk.
+        fills following lists:
+        - right_foot_height
+        - left_foot_height
+        - hip height
+        
+        The data in the above lists is ordered frame-wise.
+        The lists are accessible via own functions (see below).
+        
+        '''
         right_foot_y = []
         left_foot_y = []
         hip_height = []
         with h5py.File(self.get_path(), 'r') as param_file:
+            '''
+            following line: param_file.keys() returns sth. like 'frame_10'
+            sorting key will then be int(frame_10[6:]) resulting in 10.
+                                               ^
+                                      this is the 6th index
+            '''
             sorted_key = sorted(param_file.keys(), key=lambda x : int(x[6:]))
             for frame in sorted_key:
                 group = param_file[frame]
@@ -143,6 +186,8 @@ class ParameterFile():
                 hip_y = group["hip_height"][()]
                 hip_height.append(hip_y)
             self._hip_height = np.array(hip_height)
+            self._left_foot_pos = np.array(left_foot_y)
+            self._right_foot_pos = np.array(right_foot_y)
     
     def get_hip_height(self):
         '''
@@ -155,73 +200,35 @@ class ParameterFile():
             shape (num_frames,)
         '''
         return self._hip_height
+    
+    def get_right_foot_height(self):
+        '''
+        Relative hip height over time.
 
-    # @staticmethod
-    # def load(path: str):
-    #     right_foot_y = []
-    #     left_foot_y = []
-    #     hip_height = []
-    #     with h5py.File(path, 'r') as param_file:
-    #         sorted_key = sorted(param_file.keys(), key=lambda x : int(x[6:]))
-    #         for frame in sorted_key:
-    #             group = param_file[frame]
-    #             right_y = group["right_foot_y"][()]
-    #             right_foot_y.append(right_y)
-    #             left_y = group["left_foot_y"][()]
-    #             left_foot_y.append(left_y)
-    #             hip_y = group["hip_height"][()]
-    #             hip_height.append(hip_y)
-    #     total_error = 100
-    #     index = 0
-    #     possible_indices = []
-    #     runup_coeffs = []
-    #     jump_coeffs = []
-    #     for i in range(2, len(hip_height) - 2):
-    #         x_runup = np.arange(len(hip_height[:i]))
-    #         x_jump = np.arange(len(hip_height[i:]))
-    #         hip_fit_runup, residuals_runup, _, _, _ = np.polyfit(
-    #             x_runup,hip_height[:i], 1, full=True)
-    #         hip_fit_jump, residuals_jump, _, _, _ = np.polyfit(
-    #             x_jump, hip_height[i:], 2, full=True)
-    #         fitting_error = residuals_runup + residuals_jump
-    #         if fitting_error:
-    #             possible_indices.append(fitting_error[0])
-    #         if fitting_error < total_error:
-    #             total_error = fitting_error
-    #             index = i
-    #             runup_coeffs = hip_fit_runup
-    #             jump_coeffs = hip_fit_jump
-    #     possible_indices = np.array(possible_indices)
-    #     possible_indices = np.where(np.logical_and(
-    #         (possible_indices < (total_error + total_error*0.01)), 
-    #         (possible_indices > (total_error - total_error*0.01))))[0]
-    #     '''
-    #     TODO verify the following if statement makes sense and is reasonable.
-    #     It is meant to solve the problem that a takeoff is detected during the
-    #     runup.
-    #     '''
-    #     if(index < possible_indices[-1]):
-    #         index = possible_indices[-1]
-    #     print(f"takeoff detected at {index}")
-    #     hip_runup = np.poly1d(runup_coeffs)
-    #     hip_jump = np.poly1d(jump_coeffs)
-    #     x_runup = np.arange(0, index)
-    #     x_jump = np.arange(index, len(hip_height))
-    #     plt.xlabel("t [frames]")
-    #     plt.ylabel("height [norm. pix]")
-    #     plt.plot(hip_height, label='hip')
-    #     plt.plot(x_runup, hip_runup(x_runup), label="runup")
-    #     plt.plot(x_jump, hip_jump(np.arange(len(x_jump))), label="jump")
-    #     plt.plot(left_foot_y, label='left foot')
-    #     plt.plot(right_foot_y, label='right foot')
-    #     plt.legend()
-    #     file_name ='test.png'
-    #     plt.savefig(file_name)
+        Returns
+        -------
+        hip_height : np.ndarray
+            hip height over time as flat numpy array
+            shape (num_frames,)
+        '''
+        return self._right_foot_pos
+    
+    def get_left_foot_height(self):
+        '''
+        Relative hip height over time.
+
+        Returns
+        -------
+        hip_height : np.ndarray
+            hip height over time as flat numpy array
+            shape (num_frames,)
+        '''
+        return self._left_foot_pos
 
 class FileHandler():
     '''
     The Filehandler takes care to create the correct folder structure and
-    provides all neccessarry paths to interact with parameter files.
+    provides all necessary paths to interact with parameter files.
 
     Outputs can be found under:
     software_path/analysis/YYYY-MM-dd
@@ -260,6 +267,11 @@ class FileHandler():
         date.
         Analysis outputs are actually stored like:
         software_path/analysis/YYYY-MM-dd
+        
+        Returns
+        -------
+        Either newly created folder path or existing path (if already created
+        before)
         '''
         current_date = datetime.now().strftime("%Y-%m-%d")
         path = os.path.join(os.getcwd(), FileHandler.__OUTPUT_FOLDER
