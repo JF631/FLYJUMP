0bf6e32893a1766a7abf5a7688f6450b47b0284b
Jakob Faust <jakob.faust@curemannheim.de>
2024-01-23 19:20:21 +0100

    mainly improves plotting area

--- ui/MainWindow.py
+++ ui/MainWindow.py
@@ -12,9 +12,8 @@ Ui Files for this Window:
 Author: Jakob Faust (software_jaf@mx442.de)
 Date: 2023-10-28
 '''
-from PyQt5.QtWidgets import QMainWindow, QFileDialog, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import QMainWindow, QFileDialog, QVBoxLayout, QMessageBox
 from PyQt5.QtCore import Qt, QThreadPool, pyqtSlot
-import numpy as np
 
 from .Ui_MainWindow import Ui_MainWindow
 from .VideoProgressWidget import VideoProgressBar, VideoProgessArea
@@ -55,22 +54,40 @@ class MainWindow(QMainWindow):
         video_area.addWidget(self.video_widget)
         self.ui.main_video.setLayout(video_area)
         #matplot widget
-        self.matplot_widget = MatplotCanvas(self.ui.result_area)
+        self.matplot_widget = MatplotCanvas(self.ui.result_area,
+                                            x_label='t[frames]',
+                                            y_label='height[norm. pixel]',
+                                            control_signals=self.control_signals)
+        self.matplot_angle = MatplotCanvas(self.ui.result_area,
+                                           x_label='t[frames]',
+                                           y_label='angle[degree]',
+                                           control_signals=self.control_signals)
         result_area = QVBoxLayout(self.ui.result_area)
         result_area.addWidget(self.matplot_widget, stretch=1)
-        result_area.addWidget(QWidget(), stretch=1)
+        result_area.addWidget(self.matplot_angle, stretch=1)
         #analysis progressbar
         self.progressbar_area = VideoProgessArea(self.ui.result_area)
         result_area.addWidget(self.progressbar_area)
         self.ui.result_area.setLayout(result_area)
-        
 
     def __start_video_analaysis(self, file_names):
+        '''
+        Starts one or multiple video analysis Threads.
+        One thread per passed file name is created.
+
+        Parameters
+        ----------
+        file_names : list
+            List of video (.mp4) files that should be analyzed.
+        '''
         if not file_names:
             return
         if not self.video_widget:
             return
         self.progressbar_area.clear()
+        if self.current_video:
+            self.current_video.stop()
+            self.current_video = None
         show_video = True
         if len(file_names) > 1:
             show_video = False
@@ -91,20 +108,56 @@ class MainWindow(QMainWindow):
             self.thread_pool.start(video_task)
     
     def __show_analysis_result(self, file_names):
+        '''
+        visualizes parameter file analysis results.
+        Currently in one matplot window.
+
+        Parameters
+        ----------
+        file_names : list
+            list of hdf5 Parameter Files.
+            Only the first file in the list is visualized.
+        '''
         if not file_names:
             return
         param_file = ParameterFile(file_names[0])
         param_file.load()
         takeoff_frame = param_file.get_takeoff_frame()
+        first_frame = 0
+        if takeoff_frame is None:
+            message = QMessageBox.critical(
+                None, "Error", """No takeoff frame found. Do still you want to
+                show the other results?""",
+                QMessageBox.Yes | QMessageBox.Cancel)
+            if message == QMessageBox.Cancel:
+                return
+        else:
+            first_frame = takeoff_frame[0]
+        if self.current_video:
+            self.video_widget.disconnect_signals()
+            self.current_video.stop()
+            self.current_video = None
         self.matplot_widget.clear()
+        self.matplot_angle.clear()
         self.progressbar_area.clear()
-        self.matplot_widget.plot2D(param_file.get_left_foot_height(), label='left foot')
-        self.matplot_widget.plot2D(param_file.get_right_foot_height(), label='right foot')
-        self.matplot_widget.plot2D(param_file.get_hip_height(), label='hip height')
-        self.matplot_widget.add_points(takeoff_frame, label='changing points')
-        self.current_video = Video(param_file.get_video_path(), self.abort_flag)
+        self.matplot_widget.plot2D(param_file.get_left_foot_height(),
+                                   label='left foot')
+        self.matplot_widget.plot2D(param_file.get_right_foot_height(),
+                                   label='right foot')
+        self.matplot_widget.plot2D(param_file.get_hip_height(),
+                                   label='hip height')
+        self.matplot_widget.add_points(takeoff_frame,
+                                       label='changing points')
+        self.matplot_angle.plot2D(param_file.get_left_knee_angle(),
+                                  label='left knee angle')
+        self.matplot_angle.plot2D(param_file.get_right_knee_angle(),
+                                  label='right knee angle')
+        self.matplot_angle.add_points(takeoff_frame, label='changing points')
+        self.current_video = Video(param_file.get_video_path(),
+                                   self.abort_flag)
+        self.current_video.set_control_signals(self.control_signals)
         self.video_widget.connect_signals(self.current_video.signals)
-        self.current_video.play(takeoff_frame[0])
+        self.current_video.play(first_frame)
 
     @pyqtSlot()
     def choose_analysis_dialog(self):
@@ -137,6 +190,14 @@ class MainWindow(QMainWindow):
     
     @pyqtSlot(str)
     def analysation_finished(self, anlysis_path):
+        '''
+        Slot that is always called when a video analysis is finished.
+
+        Parameters
+        ----------
+        analysis_path : str
+            Path of the analyzed .hdf5 Parameter file
+        '''
         print(f"analysis path: {anlysis_path}")
         self.video_widget.disconnect_signals()
         self.__show_analysis_result([anlysis_path])
